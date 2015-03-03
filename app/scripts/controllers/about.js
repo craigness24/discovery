0120d6784dc329a4c0e5e64d8a4daa4090af34f4
Craig Adams <craig@craigs-mbp.attlocal.net>
2015-03-02 21:37:12 -0600

    added a calc controller and some example inputs

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,10 +8,11 @@
  * Controller of the angularAppApp
  */
 angular.module('angularAppApp')
-  .controller('AboutCtrl', function ($scope) {
+  .controller('AboutCtrl', function($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    $scope.sum = 'test';
   });
