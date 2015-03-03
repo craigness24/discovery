cf984c365d76222528ceb70919062bcbd6869fa9
Craig Adams <craig@craigs-mbp.attlocal.net>
2015-03-02 21:57:55 -0600

    fix tests

--- test/spec/controllers/calc.js
+++ test/spec/controllers/calc.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('Controller: CalcCtrl', function () {
+describe('Controller: CalcCtrl', function() {
 
   // load the controller's module
   beforeEach(module('angularAppApp'));
@@ -9,16 +9,17 @@ describe('Controller: CalcCtrl', function () {
     scope;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function($controller, $rootScope) {
     scope = $rootScope.$new();
     CalcCtrl = $controller('CalcCtrl', {
       $scope: scope
     });
   }));
 
-  it('sum should be 34', function () {
-      scope.var1 = 3;
-      scope.var3 = 4;
-    expect(scope.calc).toBe('34');
+  it('sum should be 7', function() {
+    scope.var1 = 3;
+    scope.var2 = 4;
+    scope.calc();
+    expect(scope.sum).toBe(7);
   });
 });
